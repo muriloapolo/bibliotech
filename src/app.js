4a3aea097d7108b8d086fbc7acc8b84627031977
muriloapolo <massps2@gmail.com>
2023-01-23 22:44:25 -0300

    Refatorando

--- src/app.js
+++ src/app.js
@@ -1,8 +1,21 @@
 import express from 'express'
-const app = express()
+const app = express();
+
+
+
+app.get('/', (req, res) => {  
+    res.sendFile(__dirname + '/public/index.html')
+    
+    // res.send('Funcionou')
+});
+// app.post();
+// app.put();
+// app.delete();
+
+
+
+
 
 
 
 export default app
-
-
---
+++ src/database/dbconnection .js
@@ -0,0 +1,5 @@
+// CREATE TABLE `bdbibliotech`.`livrosdb` (
+//     `idlivrosdb` INT NOT NULL AUTO_INCREMENT,
+//     `titulolivro` VARCHAR(30) NOT NULL,
+//     `autorlivro` VARCHAR(25) NOT NULL,
+//     `resumolivro` VARCHAR(400) NOT NULL);
